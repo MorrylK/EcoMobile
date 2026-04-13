c94da1995d8a280a17f23f9b746d511c9212fb29
Xybronix <xybronix@gmail.com>
2026-04-13 10:31:30 +0100

    V4

--- mobile/lib/api/config.ts
+++ mobile/lib/api/config.ts
@@ -28,7 +28,8 @@ const getApiBaseUrl = () => {
   
   // Priorité 3: URL par défaut depuis app.json
   // const defaultUrl = 'http://10.33.52.189:10000/api/v1';
-  const defaultUrl = 'https://env-freebike-xybronix.hidora.com/api/v1';
+  // const defaultUrl = 'https://env-freebike-xybronix.hidora.com/api/v1';
+  const defaultUrl = 'https://ecomobile-8bx0.onrender.com/api/v1';
   
   // En développement, toujours utiliser l'URL de développement
   if (__DEV__) {
